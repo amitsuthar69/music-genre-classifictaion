d3d67c3eff330065f4116d8c395ecabe258fe8b0
amit <sutharamit707@gmail.com>
2024-01-19 01:09:28 +0530

    external-audio-1.1

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 model = tf.keras.models.load_model('./Music-genre-classification')
 scaler = MinMaxScaler()
-label_index = {'blues': 0, 'classical': 1, 'country': 2, 'disco': 3, 'hiphop': 4, 'jazz': 5, 'metal': 6, 'pop': 7, 'reggae': 8, 'rock': 9}
+
+index_label = {'blues': 0, 'classical': 1, 'country': 2, 'disco': 3, 'hiphop': 4, 'jazz': 5, 'metal': 6, 'pop': 7, 'reggae': 8, 'rock': 9}
 
 feature_names = ['chroma_stft_mean', 'chroma_stft_var', 'rms_mean', 'rms_var', 'spectral_centroid_mean', 'spectral_centroid_var', 'spectral_bandwidth_mean', 'spectral_bandwidth_var', 'rolloff_mean', 'rolloff_var', 'zero_crossing_rate_mean', 'zero_crossing_rate_var', 'harmony_mean', 'harmony_var', 'perceptr_mean', 'perceptr_var', 'tempo', 'mfcc1_mean', 'mfcc1_var', 'mfcc2_mean', 'mfcc2_var', 'mfcc3_mean', 'mfcc3_var', 'mfcc4_mean', 'mfcc4_var', 'mfcc5_mean', 'mfcc5_var', 'mfcc6_mean', 'mfcc6_var', 'mfcc7_mean', 'mfcc7_var', 'mfcc8_mean', 'mfcc8_var', 'mfcc9_mean', 'mfcc9_var', 'mfcc10_mean', 'mfcc10_var', 'mfcc11_mean', 'mfcc11_var', 'mfcc12_mean', 'mfcc12_var', 'mfcc13_mean', 'mfcc13_var', 'mfcc14_mean', 'mfcc14_var', 'mfcc15_mean', 'mfcc15_var', 'mfcc16_mean', 'mfcc16_var', 'mfcc17_mean', 'mfcc17_var', 'mfcc18_mean', 'mfcc18_var', 'mfcc19_mean', 'mfcc19_var', 'mfcc20_mean', 'mfcc20_var']
 
@@ -114,9 +115,12 @@ async def predict_genre(audio_file: UploadFile = File(...)):
         predicted_genre_index = np.argmax(prediction)
         predicted_genre = index_label[predicted_genre_index]  # Assuming you have the label mapping
 
-        return JSONResponse(content={"predicted_genre": predicted_genre})
+        print(predicted_genre_index)
+        print(predicted_genre)
+        return predicted_genre
     except Exception as e:
-        return JSONResponse(content={"error": f"Error during prediction: {str(e)}"}, status_code=500)
+        print(e)
+        return str(e)
 
 @app.get("/")
 async def main(request: Request):
